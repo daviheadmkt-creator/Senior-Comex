749635e18b42e291730770296bd77d92ba016a72
Davi Ferreira <davi%dftarget.com.br@gtempaccount.com>
2025-09-25 13:57:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "seniorcomex-emb5q",
+  "appId": "1:265574319214:web:4d629ead2dff39e80f127f",
+  "apiKey": "AIzaSyAB8xtcSOPr_II5pOvLWh2j85YLdBHczGE",
+  "authDomain": "seniorcomex-emb5q.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "265574319214"
+};
