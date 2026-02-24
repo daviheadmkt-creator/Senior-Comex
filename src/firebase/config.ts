53a36532d0108ec3f28f56891f1bd16928a70566
Davi Ferreira <davi%dftarget.com.br@gtempaccount.com>
2026-02-24 14:42:48 +0000

    Try fixing this error: `Unhandled Runtime Error: FirebaseError: Firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "seniorcomex-emb5q",
   "appId": "1:265574319214:web:4d629ead2dff39e80f127f",
+  "storageBucket": "seniorcomex-emb5q.appspot.com",
   "apiKey": "AIzaSyAB8xtcSOPr_II5pOvLWh2j85YLdBHczGE",
   "authDomain": "seniorcomex-emb5q.firebaseapp.com",
   "measurementId": "",
